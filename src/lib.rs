e9a5f06457e49f52e2459734838a821578554233
Kevin R. Thornton <krthornt@uci.edu>
2023-03-10 17:20:46 -0800

    basic class w/a vector

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,22 @@ pub fn add(left: usize, right: usize) -> PyResult<usize> {
     Ok(left + right)
 }
 
+#[pyclass]
+struct HoldsVec {
+    data: Vec<i32>,
+}
+
+#[pymethods]
+impl HoldsVec {
+    #[new]
+    fn new(data: Vec<i32>) -> Self {
+        Self { data }
+    }
+}
+
 #[pymodule]
 fn pyo3learning(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(add, m)?)?;
+    m.add_class::<HoldsVec>()?;
     Ok(())
 }
--- test.py
+++ test.py
@@ -6,5 +6,12 @@ def test_add():
     assert x == 8
 
 
+def test_make_holder():
+    h = pyo3learning.HoldsVec([1, 2, 3])
+    # This is annoying: the repr_ns is 'builtins'
+    print(h)
+
+
 if __name__ == "__main__":
     test_add()
+    test_make_holder()
